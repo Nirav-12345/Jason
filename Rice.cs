948c8bf89155c5bee0d81fb483685739c512a8cc
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-04 00:01:04 -0800

    adding Program file

--- Rice.cs
+++ Rice.cs
@@ -4,35 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InventoryProgram2
+namespace Invent3
 {
     public class Rice
     {
+
+
+     
         public List<TypeofRice> typeofRice;
         public List<TypeofPules> typeofPules;
         public List<TypeofWheats> typeofWheats;
     }
 
-        public class TypeofRice
-        {
-            public string name;
-            public string weight;
-            public int price;
+    public class TypeofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
 
-        }
-        public class TypeofPules
-        {
-            public string name;
-            public string weight;
-            public int price;
+    }
+    public class TypeofPules
+    {
+        public string name;
+        public int weight;
+        public int price;
 
-        }
-        public class TypeofWheats
-        {
-            public string name;
-            public string weight;
-            public int price;
-        }
-    
+    }
+    public class TypeofWheats
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
 }
+
 
