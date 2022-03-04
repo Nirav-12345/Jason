b137b6cbce7f4d12d1e99302084b5c0fbfbd611c
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-04 00:00:34 -0800

    adding Program file

---
+++ Jason.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invent3
+{
+    public class Jason
+    {
+        public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Problem in reading the file");
+                    return null;
+                }
+            }
+        }
+
+    }
+}
+
+
+
+    
+
