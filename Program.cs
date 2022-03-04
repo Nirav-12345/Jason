452390d996f36d29ab08aba2b1a82e42c83ffa04
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-03 23:59:35 -0800

    adding class file

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-namespace InventoryProgram2
+namespace Invent3
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            string path = "C:/Users/Nirav Raj/Desktop/json/InventoryProgram2/Rice.json";
+            string path = "C:/Users/Nirav Raj/Desktop/json/Invent3/Rice.json";
 
-            FetchJasonRice fetchJasonForRice = new FetchJasonRice();
+            Jason fetchJasonForRice = new Jason();
             Rice data = fetchJasonForRice.Read(path);
 
             for (int i = 0; i < data.typeofRice.Count; i++)
@@ -16,7 +16,10 @@ namespace InventoryProgram2
                 Console.WriteLine(data.typeofRice[i].name);
                 Console.WriteLine(data.typeofRice[i].weight);
                 Console.WriteLine(data.typeofRice[i].price);
+                int TypeofRice = data.typeofRice[i].weight * data.typeofRice[i].price;
+                Console.WriteLine("The inventory price of Bashmuti is" + TypeofRice);
                 Console.WriteLine("----------------------");
+                
             }
             for (int i = 0; i < data.typeofPules.Count; i++)
             {
@@ -24,6 +27,9 @@ namespace InventoryProgram2
                 Console.WriteLine(data.typeofPules[i].name);
                 Console.WriteLine(data.typeofPules[i].weight);
                 Console.WriteLine(data.typeofPules[i].price);
+
+                int TypeofPulse = data.typeofPules[i].weight * data.typeofPules[i].price;
+                Console.WriteLine("The inventory price of Pulse is" + TypeofPulse);
                 Console.WriteLine("-----------------------");
             }
             for (int i = 0; i < data.typeofWheats.Count; i++)
@@ -32,6 +38,8 @@ namespace InventoryProgram2
                 Console.WriteLine(data.typeofWheats[i].name);
                 Console.WriteLine(data.typeofWheats[i].weight);
                 Console.WriteLine(data.typeofWheats[i].price);
+                int TypeofWheats = data.typeofWheats[i].weight * data.typeofWheats[i].price;
+                Console.WriteLine("The inventory price of Pulse is" + TypeofWheats);
                 Console.WriteLine("-----------------------");
             }
             //Console.WriteLine(data.weight);
